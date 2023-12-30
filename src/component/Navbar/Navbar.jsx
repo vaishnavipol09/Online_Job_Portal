a9868eab46cd2e92bfd18cabbc1539050f6e60f8
Vaishnavipol <vaishnavipol2709@gmail.com>
2023-12-30 18:03:54 +0530

    navbar completed

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Navbar = () => {
+  return (
+    <div className="h-20 flex items-center w-full text-white">
+      <h1 className="text-3xl pl-20 font-bold">Jobfinder.</h1>
+    </div>
+  )
+}
+
+export default Navbar
