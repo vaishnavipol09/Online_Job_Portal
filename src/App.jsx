fd36256dc97c9bb108d64ccddb1d1d553ba4dec5
Vaishnavipol <vaishnavipol2709@gmail.com>
2023-12-31 00:00:27 +0530

    add job card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./component/Navbar/Navbar";
 import Header from "./component/Header/Header";
 import SearchBar from "./component/SearchBar/SearchBar";
 import JobCard from "./component/JobCard/JobCard";
+import JobData from "./JobDummyData";
 
 function App() {
   
@@ -11,7 +12,9 @@ function App() {
       <Navbar />
       <Header />
       <SearchBar />
-      <JobCard />
+      {JobData.map((job)=>(
+        <JobCard key={job.id} {...job}/>
+      ))}
     </>
   )
 }
--- src/component/JobCard/JobCard.jsx
+++ src/component/JobCard/JobCard.jsx
@@ -1,25 +1,27 @@
 import dayjs from "dayjs";
 import React from "react"
 
-const JobCard = () => {
-    const skills = ["JavaScript" , "ReactJS" , "NodeJS"];
+const JobCard = (props) => {
+    //const skills = ["HTML" , "CSS" , "JavaScript" , "ReactJS"];
     const date1 = dayjs(Date.now())
-    const diffInDays = date1.diff("2023-12-29" , "day")
+    const diffInDays = date1.diff(props.postedOn , "day")
   return (
     <div className="mx-40 mb-4">
       <div className="flex justify-between items-center px-6 py-4 bg-zinc-200 rounded-md border border-black shadow-lg hover:border-blue-500 hover:translate-y-1 hover:scale-103">
         <div className="flex flex-col items-start gap-3 ">
-            <h1 className="text-lg font-semibold">Frontend Developer - Wolters kluwer</h1>
-            <p>Full Time &#x2022; Entry Level &#x2022; Onsite</p>
+            <h1 className="text-lg font-semibold">{props.title} - {props.company}</h1>
+            <p>{props.type} &#x2022; {props.experience} &#x2022; {props.location}</p>
             <div className="flex items-center gap-2">
-                {skills.map((skill) => (
+                {props.skills.map((skill) => (
                     <p key={skill} className="text-gray-500 py-1 px-2 rounded-md border border-black">{skill}</p>
                 ))}
             </div>
         </div>
         <div className="flex items-center gap-5">
-            <p className="text-gray-500">Posted {diffInDays} ago</p>
+            <p className="text-gray-500">Posted {diffInDays} days ago</p>
+            <a href={props.job_link}>
             <button className="text-blue-500 border border-blue-500 px-10 py-2 rounded-md">Apply</button>
+            </a>
         </div>
       </div>
     </div>
