ef855fbb1bd0a86814cdfccb580cdbea8c8847dd
Vaishnavipol <vaishnavipol2709@gmail.com>
2023-12-31 00:00:08 +0530

    in js add multiple data

---
+++ src/JobDummyData.js
@@ -0,0 +1,61 @@
+export default [
+    {
+        id:2,
+        postedOn:"2023-12-29",
+        title:"Backend Developer",
+        company:"TCS",
+        type:"Full Time",
+        experience:"Mid Level",
+        location:"Onsite",
+        skills:["Python" , "Java" , "Nodejs"],
+        job_link: "https://www.tcs.com/"
+    },
+
+    {
+        id:1,
+        postedOn:"2023-12-29",
+        title:"Full stack Developer",
+        company:"Infosys",
+        type:"Full Time",
+        experience:"Senior Level",
+        location:"Onsite",
+        skills:["HTML" , "CSS" , "JavaScript" , "Reactjs" , "Nodejs"],
+        job_link: "https://www.infosys.com/"
+    },
+
+    {
+        id:4,
+        postedOn:"2023-12-29",
+        title:"UX Designer",
+        company:"DCA(Design Consultant Architects)",
+        type:"Contract",
+        experience:"Entry Level",
+        location:"Remote",
+        skills:["User Research" , "User Flow" , "Empathy Mapping"],
+        job_link: "https://www.dcapl.net/"
+    },
+
+    {
+        id:5,
+        postedOn:"2023-12-29",
+        title:"Data Science",
+        company:"Amdocs",
+        type:"Full Time",
+        experience:"Senior Level",
+        location:"Remote",
+        skills:["Python" , "Machine Learning" , "Maths"],
+        job_link: "https://www.amdocs.com/"
+    },
+
+    {
+        id:6,
+        postedOn:"2023-12-29",
+        title:"HR Manager",
+        company:"HCL",
+        type:"Contract",
+        experience:"Entry Level",
+        location:"Remote",
+        skills:["Communication Skills" , "MS-Excel"],
+        job_link: "https://www.hcltech.com/"
+    },
+]
