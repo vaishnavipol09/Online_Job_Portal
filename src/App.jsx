5f53b2b6980ae739023e9583e3e19a642f90a03b
Vaishnavipol <vaishnavipol2709@gmail.com>
2023-12-31 23:39:16 +0530

    fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,25 @@ import Header from "./component/Header/Header";
 import SearchBar from "./component/SearchBar/SearchBar";
 import JobCard from "./component/JobCard/JobCard";
 import JobData from "./JobDummyData";
+import { useEffect, useState } from "react";
+import { collection, query , where , getDocs } from "firebase/firestore";
+import { db } from "./firebase.config"
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async() =>{
+    const q = query(collection(db, "jobs"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) =>{
+      //doc.data() is never undefined for query docs snapshots
+      console.log(doc.id, "=>" , doc.data());
+    })
+  }
+
+  useEffect(() =>{
+    fetchJobs()
+  },[])
   
 
   return (
